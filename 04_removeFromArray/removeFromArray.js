a70f4514b4cf72a4d96f9fafdf6d21c01286dbe4
Nimil <nimilgeorgepradeepngp@gmail.com>
2023-09-26 18:19:53 +0530

    leart filter method for 4th one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArr,...rmVal) {
+	theArr = theArr.filter(item => !rmVal.includes(item));
+	return theArr;
 };
 
 // Do not edit below this line
