0d77e4e7be03591a4f9d8d5baaabd427145db038
Nimil <nimilgeorgepradeepngp@gmail.com>
2023-09-26 17:52:36 +0530

    did 1,2,3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(theStr,num) {
+	retVal = ""
+	if(num<0){return "ERROR";}
+	for(i=0;i<num;i++){
+		retVal = retVal + theStr;
+	}
+	return retVal;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(theStr) {
+	return theStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
