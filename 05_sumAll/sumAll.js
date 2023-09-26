d73545a4f445f25fff22208fcaabf75afcda73e6
Nimil <nimilgeorgepradeepngp@gmail.com>
2023-09-26 23:54:13 +0530

    onto next one

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+	result = 0;
+	if(typeof(start)!="number" || typeof(end)!="number"){
+		return "ERROR";
+	}
 
+	if(end<start){
+		temp = end;
+		end = start;
+		start = temp;
+	}
+
+	if(start<0){
+		return "ERROR";
+	}
+
+	for(i=start;i<end+1;i++){
+		result+=i;
+	}
+	return result;
 };
 
 // Do not edit below this line
